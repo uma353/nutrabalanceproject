87f22370bf7e20c604158237b4aae376bedf261c
uma353 <gorrelauma353@gmail.com>
2025-10-27 13:18:09 +0530

    second upload

--- main/forms.py
+++ main/forms.py
@@ -5,7 +5,3 @@ class SignupForm(forms.ModelForm):
     class Meta:
         model=User
         fields=['username', 'password', 'email', 'first_name', 'last_name']
-
-        help_texts = {
-            'username': None,
-        }
